46d59c282b4a653c54547693bd49e83254a8d16e
Flavia Unzaga <100245014+flaviaunzaga@users.noreply.github.com>
2022-11-20 02:46:35 +0000

    aula 9 - estrutura de repetição, laço for

---
+++ aula3-2022-11-9.py
@@ -0,0 +1,35 @@
+print("Ola")
+
+contador = 1 
+
+#estrutura de repetição
+while(contador <=10):
+  print (contador)
+  contador = contador+1
+
+
+#contador += 1 tb funciona
+# Laço for 
+fruta = "morango"
+print (fruta)
+
+fruta1 = "morango"
+fruta2 = "laranja"
+fruta3 = "maça"
+
+#lista
+#linguagem começa contando em 0
+frutas =["morango", "melancia", "laranja"]
+
+#para exibir a terceira fruta
+print(frutas[2])
+
+#exibir quantas frutas tem na lista
+print(len(frutas)) #length
+
+#incluir fruta nova
+frutas.append("manga")
+print(len(frutas))
+print(frutas)
+
+    
